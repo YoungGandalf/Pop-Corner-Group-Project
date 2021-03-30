366b53db8e059502330b4df106567754921ff067
Dona <dthoma4@umbc.edu>
2021-03-30 14:25:58 -0400

    I added some basic tests to the tests.py. They just check that the user can be created and stored successfully. It also checks the forms when invalid data is entered. It does test that the view does not break when invalid data is placed since it should just refresh the page essentially when that does happen.

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,37 @@
+from django.urls import reverse
+
+from movies.forms import forms, UserForm
 from django.test import TestCase
+from movies.models import User
+
 
 # Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(UserEmail="testing@gmail.com", UserPassword="Testing123", UserName="testing",
+                            UserPhoneNumber="123-456-7890", IsBusiness=False)
+
+    def test_user_created(self):
+        self.assertEqual(User.objects.count(), 1)
+
+    def test_UserForm_valid(self):
+        form = UserForm(data={'UserEmail': "testing1@gmail.com", 'UserPassword': "Testing321", 'UserName': "testing1",
+                              'UserPhoneNumber': "987-654-3210", 'IsBusiness': False})
+        self.assertTrue(form.is_valid())
+
+        # Invalid Form Data
+
+    def test_UserForm_invalid(self):
+        form = UserForm(data={'UserEmail': "testing1@gmail.com", 'UserPassword': "test", 'UserName': "testing123",
+                              'UserPhoneNumber': "987-654-3210", 'IsBusiness': False})
+        self.assertFalse(form.is_valid())
+
+    def test_add_user_view(self):
+        # Invalid data fails.
+        response = self.client.post(reverse('signup'),
+                                    data={'UserEmail': "testing1@gmail.com", 'UserPassword': "",
+                                          'UserName': "testing123",
+                                          'UserPhoneNumber': "987-654-3210", 'IsBusiness': False})
+        self.assertEqual(response.status_code, 200)
+        self.failUnless(response.context['form'])
+        self.failUnless(response.context['form'].errors)
