f600cbecf742e6163d822d0ccdf11d38c4938f01
dthoma4 <70726840+dthoma4@users.noreply.github.com>
2021-04-15 21:36:58 -0400

    Update settings.py

--- PopCorner/settings.py
+++ PopCorner/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'PopCorner.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': BASE_DIR / 'Locations',
+        'NAME': BASE_DIR / 'db.sqlite3'
+    },
+    'locations':{
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'Locations'
     }
 }
 
