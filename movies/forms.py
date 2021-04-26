f6986cafc59c03a9be68c56384e9586fcf0e7128
YoungGandalf <YoungGandalfGit@gmail.com>
2021-04-26 13:57:35 -0400

    EventFrom class was duplicated, had to remove

--- movies/forms.py
+++ movies/forms.py
@@ -59,20 +59,3 @@ class EventForm(ModelForm):
     class Meta:
         model = Event
         fields = ["EventAddress", "AvailableTickets", "TotalTickets", "EventDate", "EventWebsite"]
-
-
-class EventForm(ModelForm):
-    # EventId = forms.AutoField(max_length=100, label='EventID')
-    # BusinessOwner = forms.ForeignKey(MyUser, on_delete=models.CASCADE)
-    EventAddress = forms.CharField(widget=TextInput, max_length=100, label='Address of Event')
-    # can place auto increment to decrement when bought
-    AvailableTickets = forms.IntegerField(widget=NumberInput, label="Tickets Available")
-    TotalTickets = forms.IntegerField(widget=NumberInput, label="Total Tickets")
-    EventDate = forms.DateTimeField(widget=DateTimeInput, label="Event Date")
-    # what movie to watch. So movies name/selection
-    # MovieId = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    EventWebsite = forms.URLField(widget=URLInput, max_length=100, label='Website Url')
-
-    class Meta:
-        model = Event
-        fields = ["EventAddress", "AvailableTickets", "TotalTickets", "EventDate", "EventWebsite"]
